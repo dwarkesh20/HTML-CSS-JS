bf537215eed0020797e5d57a639d3736f23a69f8
dwarkesh20 <dwarkeshdhamecha2582@gmail.com>
2023-02-12 12:30:22 +0530

    Assignment4 Program-3,4

---
+++ Java/Assignment4/3_Shape/Circle.java
@@ -0,0 +1,9 @@
+package shape;
+import java.lang.Math;
+public class Circle extends Shape {
+	
+	public void calculateArea(int value) {
+		double area = Math.PI*value*value;
+		System.out.println("Area of circle is: "+area);
+	}
+}
---
+++ Java/Assignment4/3_Shape/Square.java
@@ -0,0 +1,9 @@
+package shape;
+
+public class Square extends Shape{
+	
+	public void calculateArea(int value) {
+		double area = value*value;
+		System.out.println("Area of circle is: "+area);
+	}
+}
